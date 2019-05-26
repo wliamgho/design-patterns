5be36e54201955b0a78b855795f496bebd60f3dc
wliam06 <will.liiam06@gmail.com>
2019-05-26 18:19:45 +0700

    feat: add adapter pattern

---
+++ Structural/AdapterPlayground.playground/Contents.swift
@@ -0,0 +1,52 @@
+/*:
+ Adapter Pattern
+ --------
+ 
+ Adapter Pattern is used to provide a link between two otherwise incompatible types by wrapping the `adaptee` with a class supportes the interface required by the client.
+ 
+ 
+ ### Example:
+ */
+
+import Foundation
+protocol newSeniorStudent {
+    var age: Int { get }
+}
+
+/*:
+ Adaptee
+ */
+struct Student {
+    let id: String
+    let name: String
+    let age: Int
+
+    init(id: String, name: String, age: Int) {
+        self.id = id
+        self.name = name
+        self.age = age
+    }
+}
+
+/*:
+ Adapter
+ */
+struct NewStudent: newSeniorStudent {
+    private let student: Student
+
+    var age: Int {
+        return Int(student.age)
+    }
+
+    init(student: Student) {
+        self.student = student
+    }
+}
+
+/*:
+ Usage
+ */
+
+let student = Student(id: "123", name: "Bryant", age: 7)
+let newStudent = NewStudent(student: student)
+newStudent.age
